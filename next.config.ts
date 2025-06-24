d5c2a7edcecee350348e0c5132e7fdca48b4bc3f
arifframd <arifframadh08@gmail.com>
2025-06-24 15:56:00 +0700

    memperbarui next config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  typescript: {
+    // This will allow you to run the Next.js app even if there are TypeScript errors
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
